a5d53cde26422c01db7559f96dbf9d1296ad3faf
nik <nikaosmo@yandex.ru>
2024-11-26 13:47:30 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+	if (discriminant > 0) {
+		arr.push((- b + Math.sqrt(discriminant)) / (2 * a));
+		arr.push((- b - Math.sqrt(discriminant)) / (2 * a));
+	} else if (discriminant === 0) {
+		arr.push(- b / (2 * a));
+	} else {
+		arr.length = 0;
+	}
+	return arr;
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	percent = percent / 100;
+	let loanBody = amount - contribution;
+	let P = 1 / 12 * percent;
+	let monthlyPayment = loanBody * (P + (P / (((1 + P) ** countMonths) - 1)));
+	let totalAmount = (monthlyPayment * countMonths).toFixed(2);
+	return parseFloat(totalAmount);
 }
