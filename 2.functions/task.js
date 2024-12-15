6dcd84a3a6d1888927eef3fdeb94f8ae4a7e1024
nik <nikaosmo@yandex.ru>
2024-12-15 23:42:27 +0300

    задание готово 1.2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,6 @@
 function getArrayParams(...arr) {
-	let min = Infinity;
-	let max = -Infinity;
+	let min = arr[0];
+	let max = arr[0];
 	let sum = 0;
 	for (let i = 0; i < arr.length; i++) {
 	  let element = arr[i];
